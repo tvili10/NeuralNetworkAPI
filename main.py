c2301559763666b8ebe3fa8331df62a3ecf79d9b
tvili10 <80966514+tvili10@users.noreply.github.com>
2025-03-19 16:21:04 +0100

    Allow all origins

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from MNIST_data_handler.Datahandler import Datahandler
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
