48409001beb80a29849af4aaa663ebfbec39d0f8
tvili10 <80966514+tvili10@users.noreply.github.com>
2025-05-01 17:10:26 +0200

    Changed Procfile

--- main.py
+++ main.py
@@ -81,9 +81,3 @@ def addTrainingExample(input: TrainingExample):
     
     return {"status": "success"}
 
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.environ.get("PORT", 8000))
-    print(f"Starting server on port {port}")
-    uvicorn.run(app, host="0.0.0.0", port=port)
-
